cdc8b0e893e2ca65fb52effaa415d886da786b19
Remiing <38000675+Remiing@users.noreply.github.com>
2022-12-29 20:35:29 +0900

    capitalization.csv 추가

--- main.py
+++ main.py
@@ -14,21 +14,28 @@ if __name__ == '__main__':
     guild_members = guild_members['main_character'] + guild_members['sub_character']
 
     df_members = crawling.gather_members(guild_members)
+
     path = './_data/chart/'
     KST = timezone(timedelta(days=-1, hours=9))
     filename = datetime.now(KST).strftime('%y-%m-%d') + '.csv'
     df_members.to_csv(path + filename, index=False)
     df_members.to_csv('./_data/member_chart.csv', index=False)
+
     classification_data = classification(df_members)
     with open('./_data/total_info.yml', 'w') as file:
         yaml.dump(classification_data, file, default_flow_style=False)
     log = update_log(filename)
     with open('./_data/update_time.yml', 'a') as file:
         yaml.dump(log, file, default_flow_style=False)
+
     df_itemPrice = crawling.get_material_price()
     df_gemPrice = crawling.get_gem_price()
     df_materialPrice = pd.concat((df_itemPrice, df_gemPrice), sort=False)
     df_materialPrice.to_csv('./_data/material_price.csv', index=False)
+
     df_stepPrice = calcStepPrice()
     df_stepPrice.to_csv('./_data/step_price.csv', index=False)
+
+    df_capitalization = crawling.capitalization(df_members)
+    df_capitalization.to_csv('./_data/capitalization.csv', index=False)
 
--- src/crawling.py
+++ src/crawling.py
@@ -1,4 +1,5 @@
 import os
+import time
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -6,6 +7,7 @@ import pandas as pd
 import json
 from src.utils import load_yaml
 from dotenv import load_dotenv
+from src import auction_dict
 
 load_dotenv()
 lark_api_key = 'bearer ' + os.environ.get('lark_api_key')
@@ -39,7 +41,6 @@ def get_characterInfo(characterName):
     card = profile.select('div.profile-card__text > div > ul > li > div.card-effect__title')[-1].text
     card = card.replace(')', '').replace(' (', ',').replace('성합계', '')
     card = re.sub(r' .세트', '', card)
-    print(card)
 
     script = profile.select_one('script').text.replace(';', '').replace('$.Profile = ', '').strip()
     script = json.loads(script)
@@ -71,16 +72,17 @@ def get_characterInfo(characterName):
             equipment.append(f'{equipType}/{equipStep}/{equipLevel}/{equipQuality}')
             equipment_name.append(equipName)
         elif typeValue.split()[-1] in ['목걸이', '귀걸이', '반지']:
-            accType = typeValue.split()[-1]
+            # accType = typeValue.split()[-1]
+            accGrade, accType = re.sub('<.*?>', '', equip['Element_001']['value']['leftStr0']).split()
             accQuality = equip['Element_001']['value']['qualityValue']
             accNature = '/'.join(re.sub('[^가-힣\s]', '', equip['Element_005']['value']['Element_001']).split())
             accEngraving = []
             for i in range(2):
                 engraving = equip['Element_006']['value']['Element_000']['contentStr']['Element_00' + str(i)]['contentStr']
-                engraving = ''.join(re.findall('(?<=>)[가-힣\s]+(?=<)|\+\d{1}', engraving))
+                engraving = ''.join(re.findall('(?<=>)[가-힣\s:]+(?=<)|\+\d{1}', engraving))
                 accEngraving.append(engraving)
             accEngraving = '_'.join(accEngraving)
-            accessory.append(f'{accType}/{accQuality}/{accEngraving}/{accNature}')
+            accessory.append(f'{accType}/{accGrade}/{accQuality}/{accEngraving}/{accNature}')
 
     if gems:
         gems = sorted(gems.items(), key=lambda x: (-int(re.search(r'\d+', x[0]).group()), x[0][-1]))
@@ -156,7 +158,7 @@ def get_gem_price():
     for gemName in gemList:
         i = 1
         scale = 8
-        count = 0
+        count = 1
         price, min_list, max_list, minGem = [], [], [], 0
         while True:
             parameters = {
@@ -169,7 +171,7 @@ def get_gem_price():
                 "PageNo": i,
                 "SortCondition": "ASC"
             }
-            response = requests.post(url, headers={"authorization": lark_api_key}, data=parameters)
+            response = requests.post(url, headers={"authorization": lark_api_key}, json=parameters)
             if response.status_code == 200:
                 itemData = json.loads(response.text)
                 price = [item["AuctionInfo"]["BuyPrice"] for item in itemData['Items']]
@@ -189,6 +191,7 @@ def get_gem_price():
                 break
             elif 0 not in price:
                 if i == 1:
+                    minGem = min(price)
                     break
                 max_list = price
                 scale = int(scale / 2)
@@ -215,6 +218,71 @@ def get_gem_price():
     return df_gemPrice
 
 
+def get_acc_price(accessory_data):
+    url = "https://developer-lostark.game.onstove.com/auctions/items"
+    accessory_data = accessory_data.split('/')
+    accType = accessory_data[0]
+    accGrade = accessory_data[1]
+    accQuality = int(accessory_data[2])
+    accEng1, accEng2 = accessory_data[3].split('_')
+    accEng1_option, accEng1_value = accEng1.split('+')
+    accEng2_option, accEng2_value = accEng2.split('+')
+    accNature1 = accessory_data[4]
+    accNature2 = accessory_data[5] if len(accessory_data) > 5 else ''
+    parameters = {
+        "ItemLevelMin": 0,
+        "ItemLevelMax": 1700,
+        "ItemGradeQuality": accQuality//10*10 if accQuality != 100 else 90,
+        "EtcOptions": [
+            {
+                "FirstOption": 3,
+                "SecondOption": auction_dict.engraving_list[accEng1_option],
+                "MinValue": int(accEng1_value),
+                "MaxValue": int(accEng1_value)
+            },
+            {
+                "FirstOption": 3,
+                "SecondOption": auction_dict.engraving_list[accEng2_option],
+                "MinValue": int(accEng2_value),
+                "MaxValue": int(accEng2_value)
+            },
+            {
+                "FirstOption": 2,
+                "SecondOption": auction_dict.nature_list[accNature1]
+            },
+            {
+                "FirstOption": 2,
+                "SecondOption": auction_dict.nature_list[accNature2]
+            }
+        ],
+        "Sort": "BUY_PRICE",
+        "CategoryCode": auction_dict.category[accType],
+        "ItemTier": 3,
+        "ItemGrade": accGrade,
+        "PageNo": 0,
+        "SortCondition": "ASC"
+    }
+    accPrice = 0
+    while True:
+        response = requests.post(url, headers={"authorization": lark_api_key}, json=parameters)
+        if response.status_code != 200:
+            if 'retry-after' in response.headers:
+                time.sleep(int(response.headers['retry-after'])+1)
+                continue
+            print('error')
+            accPrice = -1
+            break
+        elif 'x-ratelimit-remaining' not in response.headers:
+            continue
+        else:
+            accData = json.loads(response.text)
+            if accData['Items']:
+                accPrice = [item["AuctionInfo"]["BuyPrice"] for item in accData['Items'] if item["AuctionInfo"]["BuyPrice"]][0]
+            break
+
+    return accPrice
+
+
 def gather_members(members):
     member_data_list = []
     global equipSetLevel
@@ -232,14 +300,98 @@ def gather_members(members):
     return df_members
 
 
-if __name__ == '__main__':
+def capitalization(df_members):
+    capitalization_data = []
+    members = df_members.to_dict('records')
+    step_price = pd.read_csv('./_data/step_price.csv')
+    step_price = step_price.to_dict('records')
+    df_itemPrice = pd.read_csv('./_data/material_price.csv')
+    gem_dict = {}
+    for gemType, gemPrice in zip(list(df_itemPrice['itemName']), list(df_itemPrice['itemPrice'])):
+        if '멸' in gemType or '홍' in gemType:
+            gem_dict[gemType] = int(gemPrice)
+
+    for member in members:
+
+        # equip
+        i, equipTotal, equipDetail = 0, 0, []
+        equips = member['equipment'].split(',')
+        for equip in equips:
+            equipPrice = 0
+            equipPart = equip.split('/')
+            if i == 0: equipType = "무기"
+            elif i == 1: equipType = "투구"
+            elif i == 2: equipType = "상의"
+            elif i == 3: equipType = "하의"
+            elif i == 4: equipType = "장갑"
+            else: equipType = "어깨"
+            equipStep = int(equipPart[1])
+            equipLevel = equipPart[2].replace('lv', '')
+            for step in step_price:
+                step_level = step['level'].replace('level_', '')
+                step_num = int(step['step'].replace('step_', ''))
+                if step_level == equipLevel and step_num > equipStep: break
+                if step_level == '1302' and step_num > 15: continue
+                if step_level == '1340' and (step_num <= 6 or step_num > 20): continue
+                if step_level == '1390' and (step_num <= 12 or step_num > 19): continue
+                if step_level == '1525' and step_num <= 12: continue
+                if i == 0:
+                    equipPrice += step['weaponAvg']
+                else:
+                    equipPrice += step['armorAvg']
+            equipTotal += equipPrice
+            equipDetail.append(f'{equipType} {equipStep} {equipPrice}')
+            i += 1
+
+        # gem
+        gemTotal, gemDetail = 0, []
+        gems = member['gem_simple'].split(',') if member['gem_simple'] == member['gem_simple'] else []
+        for gem in gems:
+            gemPart = gem.split(' x')
+            gemType = gemPart[0]
+            gemCount = int(gemPart[1])
+            gemPrice = gem_dict[gemType] * gemCount if gemType in gem_dict else 0
+            gemTotal += gemPrice
+            gemDetail.append(f'{gem} {gemPrice}')
+
+        # acc
+        accTotal, accDetail = 0, []
+        accs = member['accessory'].split(',')
+        for acc in accs:
+            accPrice = get_acc_price(acc)
+            accTotal += accPrice
+            accDetail.append(f'{acc} {accPrice}')
+
+        gemDetail = ','.join(gemDetail)
+        equipDetail = ','.join(equipDetail)
+        accDetail = ','.join(accDetail)
+        data = {
+            'name': member['name'],
+            'itemLV': member['itemLV'],
+            'accTotal': accTotal,
+            'accDetail': accDetail,
+            'gemTotal': gemTotal,
+            'gemDetail': gemDetail,
+            'equipTotal': equipTotal,
+            'equipDetail': equipDetail,
+        }
+        capitalization_data.append(data)
+        print(f'{data["name"]} accTotal:{data["accTotal"]} gemTotal:{data["gemTotal"]} equipTotal:{data["equipTotal"]}')
+    df_capitalization = pd.DataFrame(data=capitalization_data)
+
+    return df_capitalization
+
+
+# if __name__ == '__main__':
     # guild_members = load_yaml('../_data/guild_members.yml')
     # guild_members = guild_members['main_character'] + guild_members['sub_character']
     #
     # df_members = gather_members(guild_members)
+    #
+    # df_itemPrice = get_material_price()
+    # df_gemPrice = get_gem_price()
+    # df_materialPrice = pd.concat((df_itemPrice, df_gemPrice), sort=False)
+    # get_acc_price('귀걸이/고대/71/예리한 둔기+5_아드레날린+3/신속')
 
-    df_itemPrice = get_material_price()
-    df_gemPrice = get_gem_price()
-    df_materialPrice = pd.concat((df_itemPrice, df_gemPrice), sort=False)
 
 
